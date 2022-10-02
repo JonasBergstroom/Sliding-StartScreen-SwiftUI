93ba7bd0bfb3c09f7b811e56b96a2e584ecc57cd
jonasbergstrom <jontrian97@gmail.com>
2022-10-02 14:46:24 +0200

    Initial Commit

---
+++ Sliding-StartScreen-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sliding-StartScreen-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-10-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sliding-StartScreen-SwiftUI/Sliding_StartScreen_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sliding_StartScreen_SwiftUIApp.swift
+//  Sliding-StartScreen-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-10-02.
+//
+
+import SwiftUI
+
+@main
+struct Sliding_StartScreen_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
