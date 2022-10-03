fdd59e7fa9130a7d17e4a85157f973daa713c9ef
jonasbergstrom <jontrian97@gmail.com>
2022-10-03 18:15:40 +0200

    Hard-coded the "samplePages"

--- Sliding-StartScreen-SwiftUI/PageModel.swift
+++ Sliding-StartScreen-SwiftUI/PageModel.swift
@@ -15,4 +15,10 @@ struct Page: Identifiable {
     var tag: Int
     
     static var samplePage = Page(name: "Title", description: "Sample description", imageUrl: "delivery", tag: 0)
+    
+    static var samplePages: [Page] = [
+    Page(name: "Welcome to this Moving Assistance App!", description: "The best app to get your stuff moving", imageUrl: "Picture1", tag: 0),
+    Page(name: "Get the help you need!", description: "The perfect place to help each other moving stuff!", imageUrl: "Picture2", tag: 1),
+    Page(name: "Let's get started to help people out!", description: "Go ahead and register today!", imageUrl: "Picture3", tag: 2)
+    ]
 }
